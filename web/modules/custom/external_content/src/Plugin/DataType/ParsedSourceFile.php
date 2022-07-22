639b3f3e65e600ab8d36caee641bca86a821a57f
Niklan <niklanrus@gmail.com>
2022-07-22 18:49:45 +0500

    WIP on External Content.

---
+++ web/modules/custom/external_content/src/Plugin/DataType/ParsedSourceFile.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\external_content\Plugin\DataType;
+
+use Drupal\Core\TypedData\Plugin\DataType\StringData;
+use Drupal\external_content\Dto\ParsedSourceFile as ParsedSourceFileDto;
+
+/**
+ * Provides parsed source file typed data.
+ *
+ * @DataType(
+ *   id = "external_content_parsed_source_file",
+ *   label = @Translation("Parsed source file")
+ * )
+ */
+final class ParsedSourceFile extends StringData {
+
+  /**
+   * Gets parsed source file.
+   *
+   * @return \Drupal\external_content\Dto\ParsedSourceFile|null
+   *   The content document object.
+   */
+  public function getParsedSourceFile(): ?ParsedSourceFileDto {
+    if (empty($this->value)) {
+      return NULL;
+    }
+
+    // @phpcs:ignore DrupalPractice.FunctionCalls.InsecureUnserialize.InsecureUnserialize
+    return \unserialize($this->value);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setValue($value, $notify = TRUE): void {
+    if (!$value instanceof ParsedSourceFileDto) {
+      return;
+    }
+    $this->setParsedSourceFile($value, $notify);
+  }
+
+  /**
+   * Sets parsed source file value.
+   *
+   * @param \Drupal\external_content\Dto\ParsedSourceFile $parsed_source_file
+   *   The parsed source file.
+   * @param bool $notify
+   *   Indicates should parent be notified.
+   */
+  public function setParsedSourceFile(ParsedSourceFileDto $parsed_source_file, bool $notify = TRUE): void {
+    $this->value = \serialize($parsed_source_file);
+    // Notify the parent of any changes.
+    if ($notify && isset($this->parent)) {
+      $this->parent->onChange($this->name);
+    }
+  }
+
+}
---
+++ web/modules/custom/external_content/tests/src/Kernel/Plugin/DataType/ParsedSourceFileTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\external_content\Kernel\Plugin\DataType;
+
+use Drupal\Core\TypedData\TypedDataManagerInterface;
+use Drupal\external_content\Dto\ParsedSourceFile;
+use Drupal\external_content\Dto\SourceFile;
+use Drupal\external_content\Dto\SourceFileContent;
+use Drupal\external_content\Dto\SourceFileParams;
+use Drupal\Tests\external_content\Kernel\ExternalContentTestBase;
+
+/**
+ * Provides test for parsed source file data type.
+ *
+ * @coversDefaultClass \Drupal\external_content\Plugin\DataType\ParsedSourceFile
+ */
+final class ParsedSourceFileTest extends ExternalContentTestBase {
+
+  /**
+   * The typed data manager.
+   */
+  protected ?TypedDataManagerInterface $typedDataManager;
+
+  /**
+   * Tests that data type works as expected.
+   */
+  public function testDataType(): void {
+    $definition = $this->typedDataManager->createDataDefinition('external_content_parsed_source_file');
+    /** @var \Drupal\external_content\Plugin\DataType\ParsedSourceFile $typed_data */
+    $typed_data = $this->typedDataManager->create($definition);
+    self::assertNull($typed_data->getParsedSourceFile());
+
+    $typed_data->setValue('foo-bar');
+    self::assertNull($typed_data->getParsedSourceFile());
+
+    $parsed_source_file = new ParsedSourceFile(
+      new SourceFile('', ''),
+      new SourceFileParams([]),
+      new SourceFileContent(),
+    );
+    $typed_data->setValue($parsed_source_file);
+    self::assertEquals($parsed_source_file, $typed_data->getParsedSourceFile());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->typedDataManager = $this->container->get('typed_data_manager');
+  }
+
+}
