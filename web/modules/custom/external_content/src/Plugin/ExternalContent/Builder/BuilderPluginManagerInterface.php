aa1d84391c4606960d72cdfd74157863461985d3
Niklan <niklanrus@gmail.com>
2022-09-09 18:41:54 +0500

    WIP on External Content.

---
+++ web/modules/custom/external_content/src/Plugin/ExternalContent/Builder/BuilderPluginManagerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\external_content\Plugin\ExternalContent\Grouper;
+
+use Drupal\Component\Plugin\PluginManagerInterface;
+
+/**
+ * Provides an interface for builder plugin manager.
+ */
+interface BuilderPluginManagerInterface extends PluginManagerInterface {
+
+}
