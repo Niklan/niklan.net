5527edb15b72eb75b31e16a6f9d5211cc1f195e7
Niklan <niklanrus@gmail.com>
2023-02-10 22:21:14 +0500

    Move 'hash_salt' into settings.all.php.

--- assets/scaffold/settings.all.php
+++ assets/scaffold/settings.all.php
@@ -20,6 +20,25 @@ $databases['default']['default'] = [
 ];
 
 /**
+ * Salt for one-time login links, cancel links, form tokens, etc.
+ *
+ * This variable will be set to a random value by the installer. All one-time
+ * login links will be invalidated if the value is changed. Note that if your
+ * site is deployed on a cluster of web servers, you must ensure that this
+ * variable has the same value on each server.
+ *
+ * For enhanced security, you may set this variable to the contents of a file
+ * outside your document root; you should also ensure that this file is not
+ * stored with backups of your database.
+ *
+ * Example:
+ * @code
+ *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
+ * @endcode
+ */
+$settings['hash_salt'] = $_ENV['HASH_SALT'];
+
+/**
  * Location of the site configuration files.
  *
  * The $settings['config_sync_directory'] specifies the location of file system
