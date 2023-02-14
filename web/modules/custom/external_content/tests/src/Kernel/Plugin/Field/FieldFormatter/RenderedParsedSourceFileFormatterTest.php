3600c13de845866de31ab9d7b2d5ad4a8d650f71
Niklan <niklanrus@gmail.com>
2023-02-14 20:08:51 +0500

    Update chi-teck/drupal-coder-extension:2.x.

--- web/modules/custom/external_content/tests/src/Kernel/Plugin/Field/FieldFormatter/RenderedParsedSourceFileFormatterTest.php
+++ web/modules/custom/external_content/tests/src/Kernel/Plugin/Field/FieldFormatter/RenderedParsedSourceFileFormatterTest.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types = 1);
+<?php declare(strict_types = 1);
 
 namespace Drupal\Tests\external_content\Kernel\Plugin\Field\FieldType;
 
--- web/modules/custom/niklan/src/Plugin/ExtraField/Display/Term/Tag/Statistics.php
+++ web/modules/custom/niklan/src/Plugin/ExtraField/Display/Term/Tag/Statistics.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types = 1);
+<?php declare(strict_types = 1);
 
 namespace Drupal\niklan\Plugin\ExtraField\Display\Term\Tag;
 
