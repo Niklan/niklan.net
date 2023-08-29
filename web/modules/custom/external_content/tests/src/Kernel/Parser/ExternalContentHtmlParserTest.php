a499b6aacb75663dc354f10da4c3e57cc2c7694b
Niklan <niklanrus@gmail.com>
2023-08-29 19:27:30 +0500

    WIP on content sync.

--- web/modules/custom/external_content/tests/src/Kernel/Parser/ExternalContentHtmlParserTest.php
+++ web/modules/custom/external_content/tests/src/Kernel/Parser/ExternalContentHtmlParserTest.php
@@ -8,6 +8,11 @@ use Drupal\external_content\Data\ExternalContentFile;
 use Drupal\external_content\Environment\Environment;
 use Drupal\external_content\Event\HtmlPostParseEvent;
 use Drupal\external_content\Event\HtmlPreParseEvent;
+use Drupal\external_content\Node\ExternalContentDocument;
+use Drupal\external_content\Node\HtmlElement;
+use Drupal\external_content\Node\PlainText;
+use Drupal\external_content\Parser\HtmlElementParser;
+use Drupal\external_content\Parser\PlainTextParser;
 use Drupal\Tests\external_content\Kernel\ExternalContentTestBase;
 use org\bovigo\vfs\vfsStream;
 
@@ -36,7 +41,7 @@ final class ExternalContentHtmlParserTest extends ExternalContentTestBase {
 
     vfsStream::setup(structure: [
       'foo' => [
-        'bar.html' => '<p>Hello, World!</p>',
+        'bar.html' => '<p>Hello, <strong>World</strong>!</p>',
       ],
     ]);
   }
@@ -79,6 +84,52 @@ final class ExternalContentHtmlParserTest extends ExternalContentTestBase {
 
     self::assertTrue($is_pre_parse_triggered);
     self::assertTrue($is_post_parse_triggered);
+  }
+
+  /**
+   * {@selfdoc}
+   */
+  public function testParse(): void {
+    $file = new ExternalContentFile(
+      vfsStream::url('root'),
+      vfsStream::url('root/foo/bar.html'),
+    );
+
+    $environment = new Environment(new Configuration());
+    $environment->addHtmlParser(PlainTextParser::class);
+    $environment->addHtmlParser(HtmlElementParser::class);
+    $this->htmlParser->setEnvironment($environment);
+
+    $result = $this->htmlParser->parse($file);
+
+    $p = new HtmlElement('p');
+    $p->addChild(new PlainText('Hello, '));
+    $p->addChild((new HtmlElement('strong'))->addChild(new PlainText('World')));
+    $p->addChild(new PlainText('!'));
+
+    $expected_result = new ExternalContentDocument($file);
+    $expected_result->addChild($p);
+
+    self::assertEquals($expected_result, $result);
+  }
+
+  /**
+   * {@selfdoc}
+   */
+  public function testWithoutParsers(): void {
+    $file = new ExternalContentFile(
+      vfsStream::url('root'),
+      vfsStream::url('root/foo/bar.html'),
+    );
+
+    $environment = new Environment(new Configuration());
+    $this->htmlParser->setEnvironment($environment);
+
+    $result = $this->htmlParser->parse($file);
+
+    $expected_result = new ExternalContentDocument($file);
+
+    self::assertEquals($expected_result, $result);
   }
 
 }
