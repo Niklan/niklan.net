a5066b13698bbdb97ca916f3a0417c9c18e42e33
Niklan <niklanrus@gmail.com>
2023-09-01 22:07:30 +0500

    WIP on content sync.

---
+++ web/modules/custom/external_content/tests/src/Unit/Plugin/Validation/Constraint/ValidJsonConstraintValidatorTest.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\Tests\external_content\Unit\Plugin\Validation\Constraint;
+
+use Drupal\external_content\Plugin\Validation\Constraint\ValidJsonConstraint;
+use Drupal\Tests\UnitTestCase;
+use Symfony\Component\Validator\Validation;
+
+/**
+ * Provides a test for JSON validation constraint.
+ *
+ * @covers \Drupal\external_content\Plugin\Validation\Constraint\ValidJsonConstraintValidator
+ * @group external_content
+ */
+final class ValidJsonConstraintValidatorTest extends UnitTestCase {
+
+  /**
+   * {@selfdoc}
+   *
+   * @dataProvider validateData
+   */
+  public function testValidate(mixed $input, int $expected_violations): void {
+    $constraint = new ValidJsonConstraint();
+    $validator = Validation::createValidator();
+
+    $violations = $validator->validate($input, $constraint);
+    self::assertCount($expected_violations, $violations);
+  }
+
+  /**
+   * {@selfdoc}
+   */
+  public function validateData(): \Generator {
+    yield 'Not a JSON string' => [
+      'input' => 'random string',
+      'expected_violations' => 1,
+    ];
+
+    yield 'Array' => [
+      'input' => ['foo' => 'bar'],
+      'expected_violations' => 1,
+    ];
+
+    yield 'Object' => [
+      'input' => new \stdClass(),
+      'expected_violations' => 1,
+    ];
+
+    yield 'Valid JSON' => [
+      'input' => '{"foo": "bar"}',
+      'expected_violations' => 0,
+    ];
+  }
+
+}
