e8f984ad3206edb59c64b5b0e5d84c8f32c7f544
Niklan <niklanrus@gmail.com>
2023-10-19 20:14:05 +0500

    WIP on content sync.

--- web/modules/custom/niklan/src/Sync/BlogContentSyncManager.php
+++ web/modules/custom/niklan/src/Sync/BlogContentSyncManager.php
@@ -2,7 +2,12 @@
 
 namespace Drupal\niklan\Sync;
 
+use Drupal\external_content\Contract\Bundler\BundlerFacadeInterface;
+use Drupal\external_content\Contract\Environment\EnvironmentInterface;
 use Drupal\external_content\Contract\Finder\FinderFacadeInterface;
+use Drupal\external_content\Contract\Parser\HtmlParserFacadeInterface;
+use Drupal\external_content\Data\ExternalContentDocumentCollection;
+use Drupal\external_content\Data\ExternalContentFile;
 use Drupal\niklan\Builder\BlogContentEnvironmentBuilder;
 
 /**
@@ -18,14 +23,36 @@ final class BlogContentSyncManager {
   public function __construct(
     private BlogContentEnvironmentBuilder $environmentBuilder,
     private FinderFacadeInterface $finder,
+    private HtmlParserFacadeInterface $htmlParser,
+    private BundlerFacadeInterface $bundler,
   ) {}
+
+  /**
+   * {@selfdoc}
+   */
+  private function getEnvironment(): EnvironmentInterface {
+    return $this->environmentBuilder->build();
+  }
 
   /**
    * Requests content synchronization.
    */
   public function synchronize(): void {
-    $this->finder->setEnvironment($this->environmentBuilder->build());
-    $this->finder->find();
+    $this->finder->setEnvironment($this->getEnvironment());
+    $file_collection = $this->finder->find();
+
+    $this->htmlParser->setEnvironment($this->getEnvironment());
+    $content_documents = new ExternalContentDocumentCollection();
+
+    foreach ($file_collection as $content_file) {
+      \assert($content_file instanceof ExternalContentFile);
+      $content_documents->add($this->htmlParser->parse($content_file));
+    }
+
+    // @todo Add language bundler.
+    $this->bundler->setEnvironment($this->getEnvironment());
+    $content_bundles = $this->bundler->bundle($content_documents);
+    \dump($content_bundles);
     // @todo Build Queue and run it.
   }
 
