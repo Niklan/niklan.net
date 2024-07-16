21df7135ecddaee6e370035433f3f41a7f4b5fb8
Niklan <niklanrus@gmail.com>
2024-07-16 21:39:48 +0500

    WIP on new theme

--- app/Drupal/laszlo/src/Hook/Theme/InputPreprocess.php
+++ app/Drupal/laszlo/src/Hook/Theme/InputPreprocess.php
@@ -7,7 +7,7 @@ namespace Drupal\laszlo\Hook\Theme;
 final readonly class InputPreprocess {
 
   public function __invoke(array &$variables): void {
-    // @todo Figure out does it needed or not, lol.
+
   }
 
 }
