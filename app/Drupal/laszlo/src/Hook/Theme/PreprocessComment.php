b59467dafcc4a7dd42def7227a92e51df8ba4b73
Niklan <niklanrus@gmail.com>
2024-09-26 19:40:01 +0500

    WIP on new theme

---
+++ app/Drupal/laszlo/src/Hook/Theme/PreprocessComment.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\laszlo\Hook\Theme;
+
+final readonly class PreprocessComment {
+
+  public function __invoke(array &$variables): void {
+//    dump($variables);
+  }
+
+}
