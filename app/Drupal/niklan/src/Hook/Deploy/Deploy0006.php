3a063273508bf489d0d5765ef13e8078372e8d2a
Niklan <niklanrus@gmail.com>
2024-12-05 20:04:41 +0500

    Update path alias pattern
    And remove the long time ago forgotten collection.

---
+++ app/Drupal/niklan/src/Hook/Deploy/Deploy0006.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\niklan\Hook\Deploy;
+
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\QueryInterface;
+use Drupal\Core\Site\Settings;
+use Drupal\niklan\Node\Entity\BlogEntry;
+use Drupal\pathauto\PathautoState;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+final readonly class Deploy0006 implements ContainerInjectionInterface {
+
+  public function __construct(
+    private EntityTypeManagerInterface $entityTypeManager,
+  ) {}
+
+  #[\Override]
+  public static function create(ContainerInterface $container): self {
+    return new self(
+      $container->get(EntityTypeManagerInterface::class),
+    );
+  }
+
+  private function getStorage(): EntityStorageInterface {
+    return $this->entityTypeManager->getStorage('node');
+  }
+
+  private function getQuery(): QueryInterface {
+    return $this
+      ->getStorage()
+      ->getQuery()
+      ->condition('type', 'blog_entry')
+      ->accessCheck(FALSE)
+      ->sort('nid');
+  }
+
+  private function article(array &$sandbox): \Generator {
+    $ids = $this
+      ->getQuery()
+      ->range($sandbox['current'], $sandbox['limit'])
+      ->execute();
+    $sandbox['current'] += \count($ids);
+
+    yield from $this->getStorage()->loadMultiple($ids);
+  }
+
+  private function process(BlogEntry $article): void {
+    $article->set('path', ['pathauto' => PathautoState::CREATE]);
+    $article->save();
+  }
+
+  public function __invoke(array &$sandbox): string {
+    if (!isset($sandbox['total'])) {
+      $sandbox['total'] = $this->getQuery()->count()->execute();
+      $sandbox['current'] = 0;
+      $sandbox['limit'] = Settings::get('entity_update_batch_size', 50);
+    }
+
+    if (!$sandbox['total']) {
+      $sandbox['#finished'] = 1;
+
+      return 'No blog articles to process.';
+    }
+
+    foreach ($this->article($sandbox) as $media) {
+      $this->process($media);
+    }
+
+    $sandbox['#finished'] = $sandbox['current'] / $sandbox['total'];
+
+    return (string) new FormattableMarkup('@count/@total blog articles were processed.', [
+      '@count' => $sandbox['current'],
+      '@total' => $sandbox['total'],
+    ]);
+  }
+
+}
