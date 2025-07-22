64a1670556653537d21af5d125f0765badf4938a
Nikita Malyshev <niklanrus@gmail.com>
2025-07-22 21:47:13 +0500

    Remove deprecated code (#77)

--- app/modules/external_content/src/Builder/RenderArray/RenderArrayExtension.php
+++ app/modules/external_content/src/Builder/RenderArray/RenderArrayExtension.php
@@ -6,8 +6,6 @@ namespace Drupal\external_content\Builder\RenderArray;
 
 use Drupal\external_content\Contract\Extension\Extension;
 use Drupal\external_content\Extension\ContainerExtensionManager;
-use Drupal\external_content\Nodes\Format\RenderArrayBuilder as FormatBuilder;
-use Drupal\external_content\Nodes\Heading\RenderArrayBuilder as HeadingBuilder;
 use Drupal\external_content\Nodes\HtmlElement\RenderArrayBuilder as HtmlElementBuilder;
 use Drupal\external_content\Nodes\Text\RenderArrayBuilder as TextBuilder;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
@@ -26,10 +24,6 @@ final class RenderArrayExtension implements Extension {
 
   public function register(object $target): void {
     $target->add(new TextBuilder());
-    // @phpstan-ignore-next-line new.deprecatedClass
-    $target->add(new FormatBuilder());
-    // @phpstan-ignore-next-line new.deprecatedClass
-    $target->add(new HeadingBuilder());
     $target->add(new HtmlElementBuilder(), -100);
   }
 
--- app/modules/external_content/src/Parser/Array/ArrayExtension.php
+++ app/modules/external_content/src/Parser/Array/ArrayExtension.php
@@ -6,8 +6,6 @@ namespace Drupal\external_content\Parser\Array;
 
 use Drupal\external_content\Contract\Extension\Extension;
 use Drupal\external_content\Extension\ContainerExtensionManager;
-use Drupal\external_content\Nodes\Format\ArrayParser as FormatParser;
-use Drupal\external_content\Nodes\Heading\ArrayParser as HeadingParser;
 use Drupal\external_content\Nodes\HtmlElement\ArrayParser as HtmlElementParser;
 use Drupal\external_content\Nodes\Text\ArrayParser as TextParser;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
@@ -25,11 +23,7 @@ final readonly class ArrayExtension implements Extension {
   public const string ID = 'external_content.array_parser';
 
   public function register(object $target): void {
-    // @phpstan-ignore-next-line new.deprecatedClass
-    $target->add(new HeadingParser());
     $target->add(new TextParser());
-    // @phpstan-ignore-next-line new.deprecatedClass
-    $target->add(new FormatParser());
     $target->add(new HtmlElementParser());
   }
 
