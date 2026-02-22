80d87ce735311691a768c080912e4577079b1f81
Niklan <niklanrus@gmail.com>
2026-02-22 20:55:39 +0500

    refactor(laszlo): replace page header/footer theme hooks with SDC render elements

--- app/themes/laszlo/src/Hook/Theme/PreprocessPage.php
+++ app/themes/laszlo/src/Hook/Theme/PreprocessPage.php
@@ -4,13 +4,57 @@ declare(strict_types=1);
 
 namespace Drupal\laszlo\Hook\Theme;
 
+use Drupal\Core\DependencyInjection\ClassResolverInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\laszlo\Hook\Theme\PropsAlter\BrandingPropsAlter;
+use Drupal\laszlo\Hook\Theme\PropsAlter\PageFooterPropsAlter;
+use Drupal\laszlo\Hook\Theme\PropsAlter\PageHeaderPropsAlter;
 use Drupal\niklan\ExternalContent\Command\Sync;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-final readonly class PreprocessPage {
+final readonly class PreprocessPage implements ContainerInjectionInterface {
+
+  public function __construct(
+    private ClassResolverInterface $classResolver,
+  ) {}
+
+  #[\Override]
+  public static function create(ContainerInterface $container): self {
+    return new self(
+      $container->get(ClassResolverInterface::class),
+    );
+  }
 
   private function prepareHeader(array &$variables): void {
+    $headerAlter = $this->classResolver->getInstanceFromDefinition(PageHeaderPropsAlter::class);
+    $brandingAlter = $this->classResolver->getInstanceFromDefinition(BrandingPropsAlter::class);
+
     $variables['header'] = [
-      '#theme' => 'laszlo_page_header',
+      '#type' => 'component',
+      '#component' => 'laszlo:page-header',
+      '#propsAlter' => [$headerAlter(...)],
+      '#props' => [
+        'navigation' => [],
+      ],
+      '#slots' => [
+        'branding' => [
+          '#type' => 'component',
+          '#component' => 'laszlo:branding',
+          '#propsAlter' => [$brandingAlter(...)],
+          '#props' => [
+            'name' => '',
+            'url' => '',
+          ],
+        ],
+        'search' => [
+          '#type' => 'component',
+          '#component' => 'laszlo:search-bar',
+          '#props' => [
+            'placeholder' => new TranslatableMarkup('Site search'),
+          ],
+        ],
+      ],
       '#cache' => [
         'keys' => ['laszlo', 'page', 'header'],
       ],
@@ -18,8 +62,15 @@ final readonly class PreprocessPage {
   }
 
   private function prepareFooter(array &$variables): void {
+    $footerAlter = $this->classResolver->getInstanceFromDefinition(PageFooterPropsAlter::class);
+
     $variables['footer'] = [
-      '#theme' => 'laszlo_page_footer',
+      '#type' => 'component',
+      '#component' => 'laszlo:page-footer',
+      '#propsAlter' => [$footerAlter(...)],
+      '#props' => [
+        'versions' => [],
+      ],
       '#cache' => [
         'keys' => ['laszlo', 'page', 'footer'],
         'tags' => [Sync::CACHE_TAG],
--- app/themes/laszlo/src/Hook/Theme/PropsAlter/PageFooterPropsAlter.php
+++ app/themes/laszlo/src/Hook/Theme/PropsAlter/PageFooterPropsAlter.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Drupal\laszlo\Hook\Theme;
+namespace Drupal\laszlo\Hook\Theme\PropsAlter;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Site\Settings;
 use Drupal\niklan\Console\Process\GitInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-final readonly class PreprocessLaszloPageFooter implements ContainerInjectionInterface {
+final readonly class PageFooterPropsAlter implements ContainerInjectionInterface {
 
   public function __construct(
     private GitInterface $git,
@@ -19,24 +19,6 @@ final readonly class PreprocessLaszloPageFooter implements ContainerInjectionInt
   public static function create(ContainerInterface $container): self {
     return new self(
       $container->get(GitInterface::class),
-    );
-  }
-
-  private function addVersions(array &$variables): void {
-    $external_content_directory = Settings::get('external_content_directory');
-    \assert(\is_string($external_content_directory));
-    $external_content_repository_url = Settings::get('external_content_repository_url');
-    \assert(\is_string($external_content_repository_url));
-    $website_repository_url = Settings::get('website_repository_url');
-    \assert(\is_string($website_repository_url));
-
-    $variables['versions']['content'] = $this->buildVersionInfo(
-      repository_path: $external_content_directory,
-      repository_url: $external_content_repository_url,
-    );
-    $variables['versions']['website'] = $this->buildVersionInfo(
-      repository_path: \DRUPAL_ROOT,
-      repository_url: $website_repository_url,
     );
   }
 
@@ -68,8 +50,26 @@ final readonly class PreprocessLaszloPageFooter implements ContainerInjectionInt
     return \str_replace('"', '', $commit_id);
   }
 
-  public function __invoke(array &$variables): void {
-    $this->addVersions($variables);
+  public function __invoke(array $props): array {
+    $external_content_directory = Settings::get('external_content_directory');
+    \assert(\is_string($external_content_directory));
+    $external_content_repository_url = Settings::get('external_content_repository_url');
+    \assert(\is_string($external_content_repository_url));
+    $website_repository_url = Settings::get('website_repository_url');
+    \assert(\is_string($website_repository_url));
+
+    $props['versions'] = [
+      'content' => $this->buildVersionInfo(
+        repository_path: $external_content_directory,
+        repository_url: $external_content_repository_url,
+      ),
+      'website' => $this->buildVersionInfo(
+        repository_path: \DRUPAL_ROOT,
+        repository_url: $website_repository_url,
+      ),
+    ];
+
+    return $props;
   }
 
 }
