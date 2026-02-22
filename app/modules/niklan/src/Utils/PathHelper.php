b5d65ab3e5ac2e8992eeb4adb2604257cac2f54e
Niklan <niklanrus@gmail.com>
2026-02-22 20:22:33 +0500

    refactor(niklan): move extension() from FileHelper to PathHelper

--- app/modules/niklan/src/Utils/PathHelper.php
+++ app/modules/niklan/src/Utils/PathHelper.php
@@ -44,6 +44,21 @@ final class PathHelper {
     return self::normalizePathWithoutScheme($path);
   }
 
+  public static function extension(string $filename, ?string $suffix = NULL): string {
+    $extension = \pathinfo($filename, \PATHINFO_EXTENSION);
+
+    // The ".gz" extension is usually consists of two parts.
+    if ($extension === 'gz') {
+      $filename = \pathinfo($filename, \PATHINFO_FILENAME);
+      // Only continue if filename still contains dot.
+      if (\str_contains($filename, '.')) {
+        return self::extension($filename, '.gz');
+      }
+    }
+
+    return $extension . $suffix;
+  }
+
   private static function normalizePathWithoutScheme(string $path): string {
     $decoded_path = \urldecode($path);
     $is_absolute = $decoded_path !== '' && $decoded_path[0] === \DIRECTORY_SEPARATOR;
